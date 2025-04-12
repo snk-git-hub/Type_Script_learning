30e66b8ae71938ba70b28514b0a5a4e8652ca7bb
snk-git-hub <shivanandu.dev@gmail.com>
2025-04-12 14:59:41 -0400

    pick

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
+// interface User{
+//     name:string;
+//     age:number;
+// };
+// function sumOfAge(user1:User,user2:User){
+//     return user1.age+user2.age;
+// }
+
+// const age= sumOfAge({
+//     name:'Tara',
+//     age:20,
+
+// },
+// {
+//     name:'jira',
+//     age:30,
+// }
+
+// )
+
+// console.log(age)
+
+////////////////PICK///////////////////////
 interface User{
+    id:string;
     name:string;
     age:number;
+    email:string;
+    password:string;
 };
-function sumOfAge(user1:User,user2:User){
-    return user1.age+user2.age;
+
+type UpdateProps =Pick<User,'name'|'age'|'email'>
+function updateuser(updateProbs:UpdateProps){
 }
-
-const age= sumOfAge({
-    name:'Tara',
-    age:20,
-
-},
-{
-    name:'jira',
-    age:30,
-}
-
-)
-
-console.log(age)
